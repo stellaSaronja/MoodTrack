0f4e33087829b7b75391d7864ed5631d6beaa17b
stellaSaronja <88450159+stellaSaronja@users.noreply.github.com>
2022-07-15 14:35:32 +0200

    image upload in progress

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -18,7 +18,7 @@ class EntryController extends Controller
 			'mood' => 'required',
 			'activity' => 'required|max:500',
 			'challenge' => 'max:500',
-			'photo' => 'mimes:jpeg,png,jpg'
+			'photo' => 'image'
 		]);
 
 		$entry = new Entry;
